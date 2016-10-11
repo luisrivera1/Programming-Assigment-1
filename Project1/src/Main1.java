09bbcc95ddc0844ff0a2f452d2459e94a3d30913
Luisrivera1 <Luisrivera1@10.31.67.3>
2016-10-11 19:37:05 -0400

    We set up the minesweeper's grid.

--- Project1/src/Main1.java
+++ Project1/src/Main1.java
@@ -6,7 +6,7 @@ public class Main1 {
 		JFrame myFrame = new JFrame("Color Grid");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(400, 150);
-		myFrame.setSize(400, 400);
+		myFrame.setSize(400, 425);
  // 
 		MyPanel1 myPanel = new MyPanel1();
 		myFrame.add(myPanel);
